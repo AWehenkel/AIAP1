8b3ee97a98206eb7793f3a5788ecc49a46150a62
AWehenkel <antoine.wehenkel@gmail.com>
2016-10-04 10:56:43 +0200

    first commit

---
+++ dt.py
@@ -0,0 +1,30 @@
+"""
+University of Liege
+ELEN0062 - Introduction to machine learning
+Project 1 - Classification algorithms
+"""
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Only py3 / so that 2 / 3 = 0.66..
+from __future__ import division
+# Only py3 string encoding
+from __future__ import unicode_literals
+# Only py3 print
+from __future__ import print_function
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from data import make_data1, make_data2
+from sklearn.tree import DecisionTreeClassifier
+
+
+# (Question 1)
+
+# Put your funtions here
+# ...
+
+
+if __name__ == "__main__":
+    pass # Make your experiments here
