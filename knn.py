ff238fb2f9e0802d002cd1ca1dd4578cc316fa1e
antoi <antoine-dubois@hotmail.com>
2016-10-08 15:32:48 +0200

    First modif to knn

--- knn.py
+++ knn.py
@@ -22,6 +22,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 # (Question 2)
 from plot import plot_boundary
+from sklearn.model_selection import cross_val_score
 # Put your functions here
 
 def question21(n_neighbours, train_size=150, train_pos=0, title=''):
@@ -76,7 +77,7 @@ def question22(n_min, n_max):
         n_min += 1
 
 
-#!!! Pour cette question, il faudrait peut etre faire plusieurs itérations pour avoir une meilleure estimation
+#!!! Pour cette question, il faudrait peut etre faire plusieurs iterations pour avoir une meilleure estimation
 def question23(n_min, n_max):
     '''This function computes the accuracy of the k-neighbours model on the
     learning and testing sets for different number of neighbours and plot the corresponding
@@ -127,10 +128,54 @@ def question23(n_min, n_max):
         plt.plot(accu2_test)
         plt.show()
 
-def question24():
+def question24(n_max):
+    '''This function uses a ten-fold cross validation strategy to optimize the value of
+        the n_neighbors parameter for two datasets.
+
+        Parameters
+        ---------
+        n_max: int > 0
+            the range of n_neighbours parameters that are going to be tested
+
+        Return
+        ------
+        best_score: float > 0, <= 1
+            best score obtained with the ten-fold cross validation strategy
+
+        best_neigh: > 0, <= n_max
+            value of n_neighbours for which the ten-fold cross validation strategy was optimal
+
+        '''
+
+    nb_fold = 10
+
+    #Dataset 1
+    best_score1 = 0
+    best_neigh1 = 1
+    X1,y1 = make_data1(2000)
+
+    #Dataset 2
+    best_score2 = 0
+    best_neigh2 = 1
+    X2, y2 = make_data2(2000)
+
+
+    for i in range(1, n_max+1):
+        neigh = KNeighborsClassifier(n_neighbors=i)
+        score1 = sum(cross_val_score(neigh, X1, y1, cv=nb_fold))/nb_fold
+        score2 = sum(cross_val_score(neigh, X2, y2, cv=nb_fold))/nb_fold
+        #Update the return values
+        if score1 > best_score1:
+            best_score1 = score1
+            best_neigh1 = i
+
+        if score2 > best_score2:
+            best_score2 = score2
+            best_neigh2 = i
+
+    return [best_score1, best_neigh1, best_score2, best_neigh2]
 
 
 
 if __name__ == "__main__":
-
-    question23(1,50)
+    print(question24(10))
